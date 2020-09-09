2b3fc514999afc781e933d1b3209519507b441fe
Miguel Angel Larios Campos <mlariosc7@hotmail.com>
2020-09-09 16:40:13 -0600

    Error Handler

---
+++ Dapper.Application/DTOs/RequestModels/AnularAsignacionRequest.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Dapper.Application.DTOs.RequestModels
+{
+    public class AnularAsignacionRequest
+    {
+        public int IdAsignacion { get; set; }
+        public string Observaciones { get; set; }
+    }
+}
